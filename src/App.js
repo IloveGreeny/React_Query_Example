1ca840354121a2d63a596b75278671a984800063
IloveGreeny <greenxd23@gmail.com>
2022-07-16 21:52:46 +0400

    commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Characters from "./Components/Characters/Characters";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Characters/>
+    </div>
+  );
+}
+
+
